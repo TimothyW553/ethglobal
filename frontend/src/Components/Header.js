87c5e1389bb8c62440246d8802d7761aee8b39ee
daniyalmohammed <d25moham@uwaterloo.ca>
2023-06-25 08:13:40 -0400

    updating button components

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -7,7 +7,6 @@ function Header() {
     <header className="header">
       <img src={Epic} alt="Logo ting" className="real-logo" width={100} />
       <img src={Transparency} alt="React Logo" className="logo" width={200} />
-      <button type="button">Verify ID</button>
     </header>
   );
 }
